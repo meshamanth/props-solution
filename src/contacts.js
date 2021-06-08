4b6f7cb1ba6e2c372c437726242db180b8262a7a
meshamanth <shamanth.kr@gmail.com>
2021-06-08 20:32:19 +0530

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Hima Das",
+    imgURL: "https://im.rediff.com/sports/2018/apr/10hima.jpg?w=300&h=400",
+    phone: "+123 456 789",
+    email: "himadas@india.com",
+  },
+  {
+    name: "PV Sindhu",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/PV_Sindhu_headshot.jpg/220px-PV_Sindhu_headshot.jpg",
+    phone: "+987 654 321",
+    email: "sindhu@india.com",
+  },
+  {
+    name: "Ankita Raina",
+    imgURL:
+      "https://www.itftennis.com/remote.axd/media.itftennis.com/asset-bank/servlet/display?file=22137989cb680858987ffd18.jpg?crop=0.293728602552132,0.087535014005602249,0.28026766262060371,0.089635854341736612&cropmode=percentage&width=300&height=400&rnd=132525009360000000",
+    phone: "+918 372 574",
+    email: "ankita.r@india.com",
+  },
+];
+
+export default contacts;
